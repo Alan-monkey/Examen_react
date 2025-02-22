62ae1255a604c54ad783d41c185d3327f92776a5
Alan-monkey <al222310501@gmail.com>
2025-02-22 10:41:36 -0600

    Subiendo mi aplicación a AWS

--- src/App.js
+++ src/App.js
@@ -8,26 +8,46 @@ const App = () => {
   useEffect(() => {
     fetch("http://tu-servidor.com/users/")
       .then((res) => res.json())
-      .then((data) => setUsers(data));
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error al obtener usuarios:", error));
   }, []);
 
   const handleSearch = (query) => {
     fetch(`http://tu-servidor.com/users/search?query=${query}`)
       .then((res) => res.json())
-      .then((data) => setUsers(data));
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error en la búsqueda:", error));
   };
 
   return (
     <div>
       <h1>Lista de Usuarios</h1>
       <SearchBar onSearch={handleSearch} />
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} - {user.raza} - {user.edad} años
-          </li>
-        ))}
-      </ul>
+      
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Raza</th>
+            <th>Edad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length > 0 ? (
+            users.map((user, index) => (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.raza || "N/A"}</td>
+                <td>{user.edad || "N/A"}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="3" className="no-results">No hay coincidencias</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
